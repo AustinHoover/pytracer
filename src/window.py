30a639c5f49cd4f98611a5dd49213e976a90b983
AustinHoover <25714838+AustinHoover@users.noreply.github.com>
2025-09-19 13:35:31 -0400

    vscode work

--- src/window.py
+++ src/window.py
@@ -1,7 +1,18 @@
 
 import tkinter
 from collections.abc import Callable
+from typing import Final
 
+# Size of the window
+window_size: Final[int] = 400
+
+# Scaling applied to the pixels
+pixel_scaling: Final[int] = 10
+
+# Ray
+ray_dimension: Final[int] = int(window_size / pixel_scaling)
+
+# The main window for the app
 class Window:
 
     # TKinter object backing the window
@@ -13,7 +24,7 @@ class Window:
     # Inits the window
     def __init__(self) -> None:
         self.window = tkinter.Tk()
-        self.canvas = tkinter.Canvas(self.window, width=400, height=400)
+        self.canvas = tkinter.Canvas(self.window, width=window_size, height=window_size)
         self.canvas.pack()
 
         # Setup repaint function
@@ -30,8 +41,21 @@ class Window:
         canvas: tkinter.Canvas = self.canvas
         # Main repaint function
         def repaint(event: None) -> None:
-            color = "#000fff000"
-            canvas.create_rectangle(0,0,50,50,fill = color)
+            # Clear canvas
+            canvas.delete()
+            # Draw each ray
+            for x_raw in range(ray_dimension):
+                for y_raw in range(ray_dimension):
+                    x_real = x_raw * pixel_scaling
+                    y_real = y_raw * pixel_scaling
+                    color = "#000fff000"
+                    canvas.create_rectangle(
+                        x_real,
+                        y_real,
+                        pixel_scaling,
+                        pixel_scaling,
+                        fill = color
+                    )
         # Return the curried func
         return repaint
 
