1a2bfc5255d57691bf933d11455a5a7e3f7f5f5e
AustinHoover <25714838+AustinHoover@users.noreply.github.com>
2025-09-19 12:51:40 -0400

    init

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # pytracer
+
+## Setup
+
+`python -m venv .venv`
+
+`source ./.venv/Scripts/activate`
+
+`pip install -r requirements.txt`
+
+
+## Run
+
+Ideally, install VSCode, install the vscode python extension, then run main.py
---
+++ src/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Main entry point for the pytracer desktop application.
+"""
+from tkinter import *
+
+
+def main() -> None:
+    """Main function that prints hello world."""
+    print("Hello World!")
+    window = Tk()
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
